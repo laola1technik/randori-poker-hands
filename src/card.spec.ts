5ce721fbaf6d767ef1ed33f80d22be3e6a47bf75
laola1technik <laola1technik@users.noreply.github.com>
2018-10-15 15:56:35 +0200

    Cards have a value;

--- src/card.spec.ts
+++ src/card.spec.ts
@@ -1,10 +1,20 @@
-import { expect } from "chai";
+import {expect} from "chai";
 import "mocha";
 import Card from "./card";
 
 describe("Card", () => {
-  it("has value 14 if Ace", () => {
-    const card = new Card("Ad");
-    expect(card.value).to.equal(14);
-  });
+    it("has value 14 if Ace", () => {
+        const card = new Card("Ad");
+        expect(card.value).to.equal(14);
+    });
+
+    it("has value 12 if Queen", () => {
+        const card = new Card("Qc");
+        expect(card.value).to.equal(12);
+    });
+
+    it("has value 2 if Two", () => {
+        const card = new Card("2h");
+        expect(card.value).to.equal(2);
+    });
 });
--- src/card.ts
+++ src/card.ts
@@ -1,8 +1,13 @@
 export default class Card {
+    private cardsInOrder: string[];
+
     constructor(private identifier: string) {
+        this.cardsInOrder = ["noCard", "noCard",
+            "2", "3", "4", "5", "6", "7", "8", "9",
+            "T", "J", "Q", "K", "A"];
     }
 
     get value(): number {
-        return 14;
+        return this.cardsInOrder.indexOf(this.identifier[0]);
     }
 }
