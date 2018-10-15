9aa84db755fd1bd770fec00ab191881df14fef3f
laola1technik <laola1technik@users.noreply.github.com>
2018-10-15 16:01:24 +0200

    Refactor: extracts test suite.

--- src/card.spec.ts
+++ src/card.spec.ts
@@ -3,18 +3,20 @@ import "mocha";
 import Card from "./card";
 
 describe("Card", () => {
-    it("has value 14 if Ace", () => {
-        const card = new Card("Ad");
-        expect(card.value).to.equal(14);
-    });
+    describe("Value", () => {
+        it("is 14 if Ace", () => {
+            const card = new Card("Ad");
+            expect(card.value).to.equal(14);
+        });
 
-    it("has value 12 if Queen", () => {
-        const card = new Card("Qc");
-        expect(card.value).to.equal(12);
-    });
+        it("is 12 if Queen", () => {
+            const card = new Card("Qc");
+            expect(card.value).to.equal(12);
+        });
 
-    it("has value 2 if Two", () => {
-        const card = new Card("2h");
-        expect(card.value).to.equal(2);
+        it("is 2 if Two", () => {
+            const card = new Card("2h");
+            expect(card.value).to.equal(2);
+        });
     });
 });
