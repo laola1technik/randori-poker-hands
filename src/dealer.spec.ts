00e73e956860b34ba80e2b8845bdc891b0ccf014
laola1technik <laola1technik@users.noreply.github.com>
2018-11-06 16:54:06 +0100

    Refactor: Introduce dealer.

---
+++ src/dealer.spec.ts
@@ -0,0 +1,12 @@
+import {expect} from "chai";
+import "mocha";
+
+describe.skip("Dealer", () => {
+    describe("compares Hand", () => {
+        describe("of Flush", () => {
+            it("wins against high card", () => {
+                // TODO: expect(dealer.compares(flush, highCard)).to.equal(Result.WIN);
+            });
+        });
+    });
+});
